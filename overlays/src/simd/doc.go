f1cb5d3a198cf35315b6a019ce80cdb83c8c286d
Clement <clement.jean@epitech.eu>
2024-02-18 12:09:42 +0800

    comment Printfs

--- overlays/src/simd/doc.go
+++ overlays/src/simd/doc.go
@@ -6,15 +6,15 @@ func add8x16(out, a, b *[16]int8)
 
 func AddU8x16(a, b *[16]uint8) *[16]uint8 {
 	out := new([16]uint8)
-	fmt.Printf("a -> %+v\n", a)
-	fmt.Printf("b -> %+v\n", b)
-	fmt.Printf("out -> %+v\n", out)
-	fmt.Printf("------------\n")
+	//fmt.Printf("a -> %+v\n", a)
+	//fmt.Printf("b -> %+v\n", b)
+	//fmt.Printf("out -> %+v\n", out)
+	//fmt.Printf("------------\n")
 	addU8x16(out, a, b)
-	fmt.Printf("a -> %+v\n", a)
-	fmt.Printf("b -> %+v\n", b)
-	fmt.Printf("out -> %+v\n", out)
-	fmt.Printf("------------\n")
+	//fmt.Printf("a -> %+v\n", a)
+	//fmt.Printf("b -> %+v\n", b)
+	//fmt.Printf("out -> %+v\n", out)
+	//fmt.Printf("------------\n")
 	return out
 }
 
