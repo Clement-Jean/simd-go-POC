c54576e1734c51aeb9e521bb70e42abff5b50ff7
Clement <clement.jean@epitech.eu>
2024-03-13 15:38:50 +0800

    remove printf comments

--- overlays/src/simd/doc.go
+++ overlays/src/simd/doc.go
@@ -6,28 +6,12 @@ func add8x16(out, a, b *[16]int8)
 
 func AddU8x16(a, b *[16]uint8) *[16]uint8 {
 	out := new([16]uint8)
-	//fmt.Printf("a -> %+v\n", a)
-	//fmt.Printf("b -> %+v\n", b)
-	//fmt.Printf("out -> %+v\n", out)
-	//fmt.Printf("------------\n")
 	addU8x16(out, a, b)
-	//fmt.Printf("a -> %+v\n", a)
-	//fmt.Printf("b -> %+v\n", b)
-	//fmt.Printf("out -> %+v\n", out)
-	//fmt.Printf("------------\n")
 	return out
 }
 
 func Add8x16(a, b *[16]int8) *[16]int8 {
 	out := new([16]int8)
-	// fmt.Printf("a -> %+v\n", a)
-	// fmt.Printf("b -> %+v\n", b)
-	// fmt.Printf("out -> %+v\n", out)
-	// fmt.Printf("------------\n")
 	add8x16(out, a, b)
-	// fmt.Printf("a -> %+v\n", a)
-	// fmt.Printf("b -> %+v\n", b)
-	// fmt.Printf("out -> %+v\n", out)
-	// fmt.Printf("------------\n")
 	return out
 }
