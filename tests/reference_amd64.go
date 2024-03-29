1a78c91ff9fcc03cf5830d78a8e3c1ab2874023b
Clement <clement.jean@epitech.eu>
2024-03-29 13:19:56 +0800

    add stubs for other references

--- tests/reference_amd64.go
+++ tests/reference_amd64.go
@@ -25,3 +25,103 @@ func referenceAddU8x16(a, b *[16]uint8) *[16]uint8 {
 	)
 	return result
 }
+
+func referenceSaturatingAdd8x16(a, b *[16]int8) *[16]int8 {
+	result := new([16]int8)
+	return result
+}
+
+func referenceSaturatingAddU8x16(a, b *[16]uint8) *[16]uint8 {
+	result := new([16]uint8)
+	return result
+}
+
+func referenceSub8x16(a, b *[16]int8) *[16]int8 {
+	result := new([16]int8)
+	return result
+}
+
+func referenceSubU8x16(a, b *[16]uint8) *[16]uint8 {
+	result := new([16]uint8)
+	return result
+}
+
+func referenceSaturatingSub8x16(a, b *[16]int8) *[16]int8 {
+	result := new([16]int8)
+	return result
+}
+
+func referenceSaturatingSubU8x16(a, b *[16]uint8) *[16]uint8 {
+	result := new([16]uint8)
+	return result
+}
+
+func referenceAnd8x16(a, b *[16]int8) *[16]int8 {
+	result := new([16]int8)
+	return result
+}
+
+func referenceAndU8x16(a, b *[16]uint8) *[16]uint8 {
+	result := new([16]uint8)
+	return result
+}
+
+func referenceOr8x16(a, b *[16]int8) *[16]int8 {
+	result := new([16]int8)
+	return result
+}
+
+func referenceOrU8x16(a, b *[16]uint8) *[16]uint8 {
+	result := new([16]uint8)
+	return result
+}
+
+func referenceXor8x16(a, b *[16]int8) *[16]int8 {
+	result := new([16]int8)
+	return result
+}
+
+func referenceXorU8x16(a, b *[16]uint8) *[16]uint8 {
+	result := new([16]uint8)
+	return result
+}
+
+func referenceMax8x16(a, b *[16]int8) *[16]int8 {
+	result := new([16]int8)
+	return result
+}
+
+func referenceMaxU8x16(a, b *[16]uint8) *[16]uint8 {
+	result := new([16]uint8)
+	return result
+}
+
+func referenceMin8x16(a, b *[16]int8) *[16]int8 {
+	result := new([16]int8)
+	return result
+}
+
+func referenceMinU8x16(a, b *[16]uint8) *[16]uint8 {
+	result := new([16]uint8)
+	return result
+}
+
+func referenceReduceMax8x16(a *[16]int8) int8 {
+	var result int8
+	return result
+}
+
+func referenceReduceMaxU8x16(a *[16]uint8) uint8 {
+	var result uint8
+	return result
+}
+
+func referenceReduceMin8x16(a *[16]int8) int8 {
+	var result int8
+	return result
+}
+
+func referenceReduceMinU8x16(a *[16]uint8) uint8 {
+	var result uint8
+	return result
+}
