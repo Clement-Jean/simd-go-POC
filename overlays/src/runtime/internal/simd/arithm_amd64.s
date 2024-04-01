1239335c63d47ae7a17fc149bcea6e3c1b197bd3
Clement <clement.jean@epitech.eu>
2024-04-01 11:12:39 +0800

    VMOVDQA + VPADDB

--- overlays/src/runtime/internal/simd/arithm_amd64.s
+++ overlays/src/runtime/internal/simd/arithm_amd64.s
@@ -5,10 +5,10 @@ TEXT ·Add8x16(SB),NOSPLIT,$0-24
 	MOVQ out+0(FP), AX
 	MOVQ a+8(FP), BX
 	MOVQ b+16(FP), CX
-	VMOVUPS (BX), X0
-	VMOVUPS (CX), X1
-	VPADDQ X1, X0, X0
-	MOVUPD X0, (AX)
+	VMOVDQA (BX), X0
+	VMOVDQA (CX), X1
+	VPADDB X1, X0, X0
+	VMOVDQA X0, (AX)
 	RET
 
 // func SaturatingAddU8x16(out, a, b *[16]uint8)
