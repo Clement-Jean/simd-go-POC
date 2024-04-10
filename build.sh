3ceafa5621fec3ef901a15a06c5e2680f7b4937f
Clement <clement.jean@epitech.eu>
2024-04-10 10:31:33 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,46 @@
 #!/usr/bin/env bash
 
+function usage() {
+    cat <<USAGE
+
+    Usage: $0 [--skip-update]
+
+    Options:
+        -s, --skip-update:  skips Go update
+USAGE
+    exit 1
+}
+
+SKIP_UPDATE=false
+
+for arg in "$@"; do
+    case $arg in
+    -s | --skip)
+        SKIP_UPDATE=true
+		shift
+        ;;
+    -h | --help)
+        usage # run usage function on help
+        ;;
+    *)
+        usage # run usage function if wrong argument provided
+        ;;
+    esac
+done
+
 # CD into the Go source
 pushd "$(dirname -- "$0")"/go || exit
 
-# Update Go
-RELEASE_BRANCH="release-branch.go1.22"
-git fetch origin "$RELEASE_BRANCH"
-git checkout -f "$RELEASE_BRANCH"
-git reset --hard origin/"$RELEASE_BRANCH"
+if [[ $SKIP_UPDATE == false ]]; then
+	# Update Go
+	RELEASE_BRANCH="release-branch.go1.22"
+	git fetch origin "$RELEASE_BRANCH"
+	git checkout -f "$RELEASE_BRANCH"
+	git reset --hard origin/"$RELEASE_BRANCH"
+else
+	/bin/rm -rf src/simd
+	/bin/rm -rf src/runtime/internal/simd
+fi
 
 # Patch Go
 git apply --ignore-space-change --ignore-whitespace --3way ../patches/*.diff
