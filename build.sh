62ec6376b38777b5729623c2a766364d7095ac75
Clement <clement.jean@epitech.eu>
2024-04-19 13:54:52 +0800

    add possiblity to specify patch

--- build.sh
+++ build.sh
@@ -2,48 +2,55 @@
 
 function usage() {
     cat <<USAGE
+Usage: $0 [--patch patch_path] [--skip-update]
 
-    Usage: $0 [--skip-update]
-
-    Options:
-        -s, --skip-update:  skips Go update
+Options:
+  -s, --skip-update:  skips Go update
+  -p, --patch:        applies the given patch(es)
+  -h, --help:         show this help
 USAGE
     exit 1
 }
 
 SKIP_UPDATE=false
+PATCH="../patches/*.diff"
 
-for arg in "$@"; do
-    case $arg in
-    -s | --skip)
-        SKIP_UPDATE=true
-		shift
-        ;;
-    -h | --help)
-        usage # run usage function on help
-        ;;
-    *)
-        usage # run usage function if wrong argument provided
-        ;;
+while [ "$1" != "" ]; do
+    case $1 in
+		-s | --skip-update)
+			SKIP_UPDATE=true
+			;;
+		-p | --patch)
+			shift
+			PATCH=$1
+			;;
+		-h | --help)
+			usage
+			;;
+		*)
+			usage
+			exit 1
+			;;
     esac
+    shift
 done
 
 # CD into the Go source
 pushd "$(dirname -- "$0")"/go || exit
 
+RELEASE_BRANCH="release-branch.go1.22"
+
 if [[ $SKIP_UPDATE == false ]]; then
 	# Update Go
-	RELEASE_BRANCH="release-branch.go1.22"
 	git fetch origin "$RELEASE_BRANCH"
-	git checkout -f "$RELEASE_BRANCH"
-	git reset --hard origin/"$RELEASE_BRANCH"
-else
-	/bin/rm -rf src/simd
-	/bin/rm -rf src/runtime/internal/simd
 fi
 
+git checkout -f "$RELEASE_BRANCH"
+git reset --hard origin/"$RELEASE_BRANCH"
+
+
 # Patch Go
-git apply --ignore-space-change --ignore-whitespace --3way ../patches/*.diff
+git apply --ignore-space-change --ignore-whitespace --3way "$PATCH"
 cp -p -P -v -R ../overlays/* ./
 
 # Build Go
