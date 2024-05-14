0a5bc487ad1f01f49c8786336b15d18a34a35b7d
Clement <clement.jean@epitech.eu>
2024-05-14 16:29:42 +0800

    change TestMaxU8x16 to TestMax8x16

--- tests/cmp_neon_arm64_test.go
+++ tests/cmp_neon_arm64_test.go
@@ -7,28 +7,28 @@ import (
 	"testing"
 )
 
-func TestMaxU8x16(t *testing.T) {
+func TestMax8x16(t *testing.T) {
 	tests := []struct {
-		a [16]uint8
-		b [16]uint8
+		a [16]int8
+		b [16]int8
 	}{
 		{
-			a: [16]uint8{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
-			b: [16]uint8{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
+			a: [16]int8{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
+			b: [16]int8{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
 		},
 		{
-			a: [16]uint8{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
-			b: [16]uint8{16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
+			a: [16]int8{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
+			b: [16]int8{16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
 		},
 		{
-			a: *(*[16]uint8)(genUint8Arr(16)),
-			b: *(*[16]uint8)(genUint8Arr(16)),
+			a: *(*[16]int8)(genInt8Arr(16)),
+			b: *(*[16]int8)(genInt8Arr(16)),
 		},
 	}
 
 	for _, test := range tests {
-		expected := referenceMaxU8x16(test.a, test.b)
-		got := simd.MaxU8x16(test.a, test.b)
+		expected := referenceMax8x16(test.a, test.b)
+		got := simd.Max8x16(test.a, test.b)
 
 		for i := 0; i < len(got); i++ {
 			if expected[i] != got[i] {
