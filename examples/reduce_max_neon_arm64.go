2be136cfc4cbb1ea27e4472e6c60817bdc590d02
Clement <clement.jean@epitech.eu>
2024-05-14 16:27:30 +0800

    remove fixed size array in simple examples

--- examples/reduce_max_neon_arm64.go
+++ examples/reduce_max_neon_arm64.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	a := [16]uint8{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+	a := simd.Uint8x16{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
 
 	fmt.Printf("%d\n", simd.ReduceMaxU8x16(a))
 }
